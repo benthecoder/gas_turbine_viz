4d3c7e7eea9281e324f9fe861ffae2d68566f121
benthecoder <benthecoder07@gmail.com>
2022-10-09 18:15:51 -0500

    update readme

--- README.md
+++ README.md
@@ -5,16 +5,16 @@
 ### connect to cockroach
 
 ```sql
-cockroach sql --url "postgresql://benedict<PASSWORD>@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dshard-oyster-5558"
+cockroach sql --url "postgresql://<USER><PASSWORD>@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dshard-oyster-5558"
 ```
 
 ### import data from s3 csv files
 
 ```sql
 IMPORT INTO ENGINE(location_id,datetime,CMP_SPEED,POWER, FUEL_FLOW,CO2,CUSTOMER_NAME,PLANT_NAME,ENGINE_ID)
-CSV DATA ('https://hackathonfiles123.s3.amazonaws.com/all_data.csv');
+CSV DATA ('https://hackathonfiles123.s3.amazonaws.com/all_data.csv')  WITH skip = '1';
 ```
 
 ```sql
-IMPORT INTO SITE(CUSTOMER_NAME,PLANT_NAME,LATITUDE,LONGITUDE,ELEVATION,FUEL_N2_MOL_PCT,FUEL_MW,FUEL_LHV,CO2_FUEL_RATIO) CSV DATA ('https://hackathonfiles123.s3.amazonaws.com/site_metadata.csv');
+IMPORT INTO SITE(CUSTOMER_NAME,PLANT_NAME,LATITUDE,LONGITUDE,ELEVATION,FUEL_N2_MOL_PCT,FUEL_MW,FUEL_LHV,CO2_FUEL_RATIO) CSV DATA ('https://hackathonfiles123.s3.amazonaws.com/site_metadata.csv')  WITH skip = '1';
 ```
--- utils/csv2cockroachdb.py
+++ utils/csv2cockroachdb.py
@@ -1,37 +1,55 @@
 import os
 import psycopg2
+from dotenv import load_dotenv
 
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
+load_dotenv()
+PASSWORD = os.getenv("COCKROACH_PW")
+
+conn = psycopg2.connect(
+    host="free-tier14.aws-us-east-1.cockroachlabs.cloud",
+    port=26257,
+    user="benedict",
+    password=PASSWORD,
+    sslmode="require",
+    sslrootcert="./root.crt",
+    database="defaultdb",
+    options="--cluster=shard-oyster-5558",
+)
 
 with conn.cursor() as cur:
 
     sql1 = """
+    DROP TABLE IF EXISTS ENGINE
     CREATE TABLE ENGINE(
-        location_id VARCHAR,
-        datetime VARCHAR,
-        CMP_SPEED VARCHAR,
-        POWER VARCHAR,
-        FUEL_FLOW VARCHAR,
-        CO2 VARCHAR,
+        location_id INT,
+        datetime TIMESTAMP,
+        CMP_SPEED FLOAT,
+        POWER FLOAT,
+        FUEL_FLOW FLOAT,
+        CO2 FLOAT,
         CUSTOMER_NAME VARCHAR,
         PLANT_NAME VARCHAR,
-        ENGINE_ID VARCHAR
+        ENGINE_ID INT
     );
     """
 
-    # cur.execute(sql1)
+    cur.execute(sql1)
 
     sql2 = """
+        DROP TABLE IF EXISTS SITE
         CREATE TABLE SITE(
             CUSTOMER_NAME VARCHAR,
             PLANT_NAME VARCHAR,
-            LATITUDE VARCHAR,
-            LONGITUDE VARCHAR,
-            ELEVATION VARCHAR,
-            FUEL_N2_MOL_PCT VARCHAR,
-            FUEL_MW VARCHAR,
-            FUEL_LHV VARCHAR,
-            CO2_FUEL_RATIO VARCHAR
+            LATITUDE FLOAT,
+            LONGITUDE FLOAT,
+            ELEVATION FLOAT,
+            FUEL_N2_MOL_PCT FLOAT,
+            FUEL_MW FLOAT,
+            FUEL_LHV FLOAT,
+            CO2_FUEL_RATIO FLOAT,
+            CONTINENT VARCHAR,
+            COUNTRY_CODE VARCHAR,
+            COUNTRY VARCHAR
     );
     """
 
